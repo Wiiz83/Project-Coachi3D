f471d9a70576fefa96114bb96212a4a71fdb3b01
mh0 <mahdi.hentati@gmail.com>
2018-05-27 00:10:10 +0200

    amélioration saut de l'animal

--- CoachiUnity/Assets/AnimalControllerScript.cs
+++ CoachiUnity/Assets/AnimalControllerScript.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 using UnityEngine.AI;
 public class AnimalControllerScript : MonoBehaviour {
 	[SerializeField] Animator anim;
-	private Rigidbody rb;
+	//private Rigidbody rb;
 	// navigation
+	float speed = 0.5f;
+	bool isStopped = true;
+	private bool jumped = false;
+	private bool isOnBed = false;
 	NavMeshAgent agent;
 	// positions
 	Vector3 center_point;
 	Vector3 bed1_point;
 	Vector3 bed2_point;
-	Vector3 destination;
-	bool isStopped = true;
 
-	float speed = 0.5f;
-
-	// Use this for initialization
 	void Start () {
-		loadPoints ();
-		rb = GetComponent<Rigidbody> ();
+		//rb = GetComponent<Rigidbody> ();
+		center_point = GameObject.Find ("center_point").transform.position;
+		bed1_point = GameObject.Find ("bed1_point").transform.position;
+		bed2_point = GameObject.Find ("bed2_point").transform.position;
 		agent = GetComponent<NavMeshAgent> ();
 		anim_lieDown ();
+		agent.autoTraverseOffMeshLink = false;
+		isOnBed = isUp ();
 	}
 
-	// Update is called once per frame
 	void Update () {
 
 		// move
@@ -48,7 +50,7 @@ public class AnimalControllerScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.F8))
 			anim_twoFeet ();
 
-		//Actions
+		// action
 		if (Input.GetKeyDown (KeyCode.F9))
 			anim_bark ();
 		if (Input.GetKeyDown (KeyCode.F10))
@@ -56,11 +58,30 @@ public class AnimalControllerScript : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		// detect stopped
-		if (Vector3.Distance (transform.position, agent.destination) <= 1f) {
+		// Agent reached destination
+		if (Vector3.Distance (transform.position, agent.destination) <= 0.5f) {
 			if (!isStopped) {
-				anim_stop ();
-				isStopped = true;
+				stopAnimal ();
+			} else if (isOnBed) {
+				anim_lieDown ();
+			}
+		}
+
+		// Agent reached jump/drop point
+		if (agent.isOnOffMeshLink) {
+			if (!jumped && !isOnBed) { // Don't jump if already jumped or dropping from bed
+				anim_jump ();
+				jumped = true;
+			}
+			// do jump / drop
+			OffMeshLinkData data = agent.currentOffMeshLinkData;
+			Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+			agent.transform.position = Vector3.MoveTowards (agent.transform.position, endPos, agent.speed * Time.deltaTime);
+			// Agent reached jump/drop target point
+			if (agent.transform.position == endPos) {
+				agent.CompleteOffMeshLink ();
+				jumped = false;
+				isOnBed = isUp ();
 			}
 		}
 	}
@@ -69,7 +90,6 @@ public class AnimalControllerScript : MonoBehaviour {
 	public void moveTo (Vector3 destination) {
 		isStopped = false;
 		anim_walk ();
-		this.destination = destination;
 		agent.destination = destination;
 	}
 
@@ -83,18 +103,23 @@ public class AnimalControllerScript : MonoBehaviour {
 		moveTo (center_point);
 	}
 
+	public void stopAnimal () {
+		stopMovement ();
+		anim_speed (0);
+		anim_stand ();
+	}
+
 	public void stopMovement () {
-		if (isStopped)
-			return;
-		moveTo (transform.position);
+		isStopped = true;
+		agent.destination = agent.transform.position;
+	}
+
+	private bool isUp () {
+		return agent.transform.position.y > 1.1f;
 	}
 
 	// Animation 
 
-	public void anim_stop () {
-		anim_speed (0);
-		anim_stand ();
-	}
 	public void anim_stand () {
 		anim_idleState (0);
 	}
@@ -128,13 +153,6 @@ public class AnimalControllerScript : MonoBehaviour {
 	}
 	private void anim_trigger (String trigger) {
 		anim.SetTrigger (trigger);
-	}
-
-	// positions
-	private void loadPoints () {
-		center_point = GameObject.Find ("center_point").transform.position;
-		bed1_point = GameObject.Find ("bed1_point").transform.position;
-		bed2_point = GameObject.Find ("bed2_point").transform.position;
 	}
 
 }
