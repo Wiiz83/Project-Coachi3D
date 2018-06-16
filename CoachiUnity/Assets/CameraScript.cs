a3984b2ac9ee193ba8f408d0e3b9b16409596db3
mh0 <mahdi.hentati@gmail.com>
2018-06-16 15:59:48 +0200

    amélioration camera

--- CoachiUnity/Assets/CameraScript.cs
+++ CoachiUnity/Assets/CameraScript.cs
@@ -1,21 +1,63 @@
-﻿﻿using System.Collections;
+﻿﻿//SmoothLookAt.cs
+//Written by Jake Bayer
+//Written and uploaded November 18, 2012
+//This is a modified C# version of the SmoothLookAt JS script.  Use it the same way as the Javascript version.
+
+using System.Collections;
 using UnityEngine;
 
+///<summary>
+///Looks at a target
+///</summary>
+[AddComponentMenu ("Camera-Control/Smooth Look At CS")]
 public class CameraScript : MonoBehaviour {
+    public Transform target; //an Object to lock on to
+    public float damping = 6.0f; //to control the rotation 
+    public bool smooth = true;
+    public float minDistance = 5.0f; //How far the target is from the camera
+    public string property = "";
 
-    public GameObject player;
+    private Color color;
+    private float alpha = 1.0f;
+    private Transform _myTransform;
 
-    private Vector3 offset; //Private variable to store the offset distance between the player and camera
+    private Vector3 offset;
 
-    // Use this for initialization
-    void Start () {        
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+    void Awake () {
+        _myTransform = transform;
     }
 
-    // LateUpdate is called after Update each frame
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
     void LateUpdate () {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        if (target) {
+            if (smooth) {
+
+                //Look at and dampen the rotation
+                Quaternion rotation = Quaternion.LookRotation (target.position - _myTransform.position);
+                _myTransform.rotation = Quaternion.Slerp (_myTransform.rotation, rotation, Time.deltaTime * damping);
+              //   _myTransform.position = target.transform.position + offset;
+            } else { //Just look at
+                _myTransform.rotation = Quaternion.FromToRotation (-Vector3.forward, (new Vector3 (target.position.x, target.position.y, target.position.z) - _myTransform.position).normalized);
+
+                float distance = Vector3.Distance (target.position, _myTransform.position);
+
+                if (distance < minDistance) {
+                    alpha = Mathf.Lerp (alpha, 0.0f, Time.deltaTime * 2.0f);
+                   
+                } else {
+                    alpha = Mathf.Lerp (alpha, 1.0f, Time.deltaTime * 2.0f);
+
+                }
+                
+            }
+        }
     }
 }
